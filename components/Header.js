66391521ca70190be4754adc82ae6e3bff362d6f
whs465 <whs465@gmail.com>
2023-02-13 17:46:29 +0400

    add sticky Header

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,5 @@
+import { PlusCircleIcon, SearchIcon } from "@heroicons/react/outline";
+import { HomeIcon } from "@heroicons/react/solid";
 import Image from "next/image";
 
 export default function Header() {
@@ -19,6 +21,37 @@ export default function Header() {
             className="object-contain"
             onClick={() => router.push("/")}
           />
+        </div>
+
+        <div className="relative mt-1">
+          <div className="absolute top-2 left-2">
+            <SearchIcon className="h-5 text-gray-500" />
+          </div>
+          <input
+            type="text"
+            placeholder="Search"
+            className="bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md"
+          />
+        </div>
+        {/* Right */}
+
+        <div className="flex space-x-4 items-center">
+          <HomeIcon
+            onClick={() => router.push("/")}
+            className="hidden md:inline-flex h-6 cursor-pointer hover:scale-125 transition-tranform duration-200 ease-out"
+          />
+
+          <>
+            <PlusCircleIcon
+              onClick={() => setOpen(true)}
+              className="h-6 cursor-pointer hover:scale-125 transition-tranform duration-200 ease-out"
+            />
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/800px-Instagram_logo_2016.svg.png"
+              alt="user-image"
+              className="h-10 rounded-full cursor-pointer"
+            />
+          </>
         </div>
       </div>
     </div>
