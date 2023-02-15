3025c35e54108b211372ab94b8e4e92ada247c2f
whs465 <whs465@gmail.com>
2023-02-15 11:12:44 +0400

    signin / out with google

--- components/Header.js
+++ components/Header.js
@@ -1,27 +1,30 @@
 import { PlusCircleIcon, SearchIcon } from "@heroicons/react/outline";
 import { HomeIcon } from "@heroicons/react/solid";
+import { signIn, signOut, useSession } from "next-auth/react";
 import Image from "next/image";
 
 export default function Header() {
+  const { data: session } = useSession();
+  console.log(session);
   return (
     <div className="shadow-sm border-b sticky top-0 bg-white z-30">
       <div className="flex items-center justify-between max-w-6xl mx-4 xl:mx-auto">
+        {/* Left */}
         <div className="cursor-pointer h-24 w-24 relative hidden lg:inline-grid">
           <Image
             src="http://www.jennexplores.com/wp-content/uploads/2015/09/Instagram_logo_black.png"
-            fill
+            layout="fill"
             className="object-contain"
-            onClick={() => router.push("/")}
           />
         </div>
         <div className="cursor-pointer h-24 w-10 relative  lg:hidden">
           <Image
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/800px-Instagram_logo_2016.svg.png"
-            fill
+            layout="fill"
             className="object-contain"
-            onClick={() => router.push("/")}
           />
         </div>
+        {/* Middle */}
 
         <div className="relative mt-1">
           <div className="absolute top-2 left-2">
@@ -33,25 +36,24 @@ export default function Header() {
             className="bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md"
           />
         </div>
+
         {/* Right */}
 
         <div className="flex space-x-4 items-center">
-          <HomeIcon
-            onClick={() => router.push("/")}
-            className="hidden md:inline-flex h-6 cursor-pointer hover:scale-125 transition-tranform duration-200 ease-out"
-          />
-
-          <>
-            <PlusCircleIcon
-              onClick={() => setOpen(true)}
-              className="h-6 cursor-pointer hover:scale-125 transition-tranform duration-200 ease-out"
-            />
-            <img
-              src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/800px-Instagram_logo_2016.svg.png"
-              alt="user-image"
-              className="h-10 rounded-full cursor-pointer"
-            />
-          </>
+          <HomeIcon className="hidden md:inline-flex  h-6 cursor-pointer hover:scale-125 transition-tranform duration-200 ease-out" />
+          {session ? (
+            <>
+              <PlusCircleIcon className="h-6 cursor-pointer hover:scale-125 transition-tranform duration-200 ease-out" />
+              <img
+                onClick={signOut}
+                src={session.user.image}
+                alt="user-image"
+                className="h-10 rounded-full cursor-pointer"
+              />
+            </>
+          ) : (
+            <button onClick={signIn}>Sign in</button>
+          )}
         </div>
       </div>
     </div>
