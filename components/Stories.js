85240a847408af5ed46071b6f86b79e66f348f87
whs465 <whs465@gmail.com>
2023-02-15 10:26:49 +0400

    adding firebase

---
+++ components/Stories.js
@@ -0,0 +1,29 @@
+import minifaker from "minifaker";
+import "minifaker/locales/en";
+import { useEffect, useState } from "react";
+import Story from "./Story";
+
+export default function Stories() {
+  const [storyUsers, setStoryUsers] = useState([]);
+  useEffect(() => {
+    const storyUsers = minifaker.array(20, (i) => ({
+      username: minifaker.username({ locale: "en" }).toLowerCase(),
+      img: `https://i.pravatar.cc/150?img=${Math.ceil(Math.random() * 70)}`,
+      id: i,
+    }));
+    setStoryUsers(storyUsers);
+    console.log(storyUsers);
+  }, []);
+  return (
+    <div className="flex space-x-2 p-6 bg-white mt-8 border-gray-200 border overflow-x-scroll rounded-sm scrollbar-none">
+      {/* <Story
+        img={currentUser?.userImg}
+        username={currentUser?.username}
+        isUser="true"
+      /> */}
+      {storyUsers.map((user) => (
+        <Story key={user.id} username={user.username} img={user.img} />
+      ))}
+    </div>
+  );
+}
